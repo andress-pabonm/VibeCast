888c69a305d874a393234593ee0320c3ac437759
andress-pabonm <andress.pabonm@ucuenca.edu.ec>
2025-06-19 23:15:33 -0500

    Implementé una función tipo printf para alojar un string en HEAP

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,46 +1,23 @@
 #include <utils/utils.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> // Para malloc
+#include <string.h> // Para memcpy
+#include <stdarg.h> // Para va_list
+#include <stdio.h>  // Para snprintf
 
-void *mem_alloc_copy(size_t size, const void *src)
+void *malloc_cpy(size_t size, const void *src)
 {
     void *dest = size ? malloc(size) : NULL;
     return (dest && src) ? memcpy(dest, src, size) : dest;
 }
 
-int scan(const char *fmt, ...)
+char *mprintf(const char *fmt, ...)
 {
+    char buffer[1024] = {0};
+
     va_list args;
     va_start(args, fmt);
-    int r = vscanf(fmt, args);
-    va_end(args);
-    flush();
-    return r;
-}
 
-int menu(const char title[], int opcc, const char *opcv[])
-{
-    if (opcc < 1 || !opcv)
-        return 0;
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
 
-    if (title)
-        puts(title);
-
-    int opc, inputCount;
-
-    for (opc = 0; opc < opcc; opc++)
-        printf("%d. %s\n", opc + 1, opcv[opc]);
-
-    do
-    {
-        printf("Elige una opción: ");
-        inputCount = scan("%d", &opc);
-
-        if (!inputCount)
-            puts("Entrada no válida.");
-    } while (!inputCount);
-
-    return opc;
+    return malloc_cpy(strlen(buffer) + 1, buffer);
 }
