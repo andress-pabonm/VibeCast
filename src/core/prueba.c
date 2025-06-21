18f0f82bc4c5624d511ed071f9cdc7c08fbd4323
LeonardoByte <kirman.sanchez@ucuenca.edu.ec>
2025-06-20 22:48:39 -0400

    Es solo una prueba.

--- src/core/prueba.c
+++ src/core/prueba.c
@@ -1,1 +1,10 @@
 /* Este es un archivo de prueba */
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hola, mundo!\n");
+    return 0;
+}
+// Este es un comentario de una sola línea
