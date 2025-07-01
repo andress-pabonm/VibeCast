b172817058ebc0eabf23f9166c1bfa723da184dd
JmateoAF <jostynmatius44@gmail.com>
2025-06-30 22:01:18 -0500

    Mejorando perfil.html

--- src/ui/ColadeReproduccion.c
+++ src/ui/ColadeReproduccion.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #include <db/estructuras.h>
-#include <nodos.h>
  
 //AQUI ESTABAN TODAS LAS ESTRUCTURAS DE ESTRUCTURAS.H
 
@@ -18,20 +17,20 @@ typedef enum {
 //     // Puntero al siguiente nodo
 //     NodoAnuncio *siguiente;
 // };
-// struct NodoCola {
-//     void* dato;                 // Puntero GENÉRICO. Apuntará a una CancionGobal o a un Anuncio.
-//     ElementoColaTipo tipo;      // La ETIQUETA que nos dice qué es 'dato'.
-//     struct NodoCola* siguiente; // Puntero al siguiente elemento en la cola.
-// } ;
-// struct NodoColadeReproduccion
-// {
-// 	NodoCola* frente;  // Apunta al primer elemento (el que saldrá primero).
-// 	NodoCola* final;   // Apunta al último elemento (para agregar nuevos fácilmente).
-// 	int contadorCancionesFree; 
-// };
+
+typedef struct NodoCola {
+    void *dato;                 // Puntero GENÉRICO. Apuntará a una CancionGobal o a un Anuncio.
+    ElementoColaTipo tipo;      // La ETIQUETA que nos dice qué es 'dato'.
+}NodoCola;
+
+typedef struct NodoColadeReproduccion {
+	NodoCola* frente;  // Apunta al primer elemento (el que saldrá primero).
+	NodoCola* final;   // Apunta al último elemento (para agregar nuevos fácilmente).
+	int contadorCancionesFree; 
+}NodoColadeReproduccion;
 
 void insertarAnuncioCircular(Lista *inicioLista, Anuncio *anuncio) {
-    //DOCUMENTADO HECHO POR PAULA Y LEO, INCERTAR ANUNCIOS COMO LISTA CIRCULAR
+    //DOCUMENTADO HECHO POR PAULA Y LEO, INSERTAR ANUNCIOS COMO LISTA CIRCULAR
     // NodoAnuncio *nuevo = (NodoAnuncio *)malloc(sizeof(NodoAnuncio));
 
     //ANUNCIOS INCERTADOS COMO LISTA 
@@ -56,47 +55,52 @@ void insertarAnuncioCircular(Lista *inicioLista, Anuncio *anuncio) {
     // }
 }
 
-void encolar(Cola *colaReproduccion, void* dato, ElementoColaTipo tipo) {
-    insertValueInCola(colaReproduccion, )
+void encolar(Cola *colaReproduccion, void *dato, ElementoColaTipo tipo) {
+    //DOCUMENTADO HECHO POR PAULA Y LEO, ENCOLAR
+    NodoCola *nuevoNodo = (NodoCola*)malloc(sizeof(NodoCola));
 
-    NodoCola* nuevoNodo = (NodoCola*)malloc(sizeof(NodoCola));
     if (!nuevoNodo) return; // Manejo de error en caso de falla de memoria
 
     nuevoNodo->dato = dato;
     nuevoNodo->tipo = tipo;
-    nuevoNodo->siguiente = NULL;
 
-    if (cola->frente == NULL) {
-        // Cola vacía, el nuevo nodo es tanto el frente como el final
-        cola->frente = nuevoNodo;
-        cola->final = nuevoNodo;
-    } else {
-        // Encolar al final
-        cola->final->siguiente= nuevoNodo;
-        cola->final = nuevoNodo;
-    }
+    insertValueInCola(colaReproduccion, nuevoNodo);
+
+    // if (cola->frente == NULL) {
+    //     // Cola vacía, el nuevo nodo es tanto el frente como el final
+    //     cola->frente = nuevoNodo;
+    //     cola->final = nuevoNodo;
+    // } else {
+    //     // Encolar al final
+    //     cola->final->siguiente= nuevoNodo;
+    //     cola->final = nuevoNodo;
+    // }
 }
-void agregarCancionACola(NodoColadeReproduccion* cola, Usuario* usuario, Cancion* cancion, NodoAnuncio* cabezaAnuncio) {
+
+void agregarCancionACola(NodoColadeReproduccion *colaReproduccion, Usuario* usuario, Cancion *cancion, Anuncio *cabezaAnuncio) {
     // 1. Siempre se agrega la canción a la cola.
     // Se usa la función 'encolar' interna, pasándole la canción y su TIPO.
-    encolar(cola, cancion, TIPO_CANCION);
+    encolar(colaReproduccion, cancion, TIPO_CANCION);
 
     // 2. Se aplica la lógica SOLO para usuarios free.
     if (usuario->plan == PLAN_FREEMIUM) {
-        cola->contadorCancionesFree++; // 3. Se incrementa el contador.
+        colaReproduccion->contadorCancionesFree++; // 3. Se incrementa el contador.
 
         // 4. Se revisa si es momento de un anuncio.
-        if (cola->contadorCancionesFree == 3) {
+        if (colaReproduccion->contadorCancionesFree == 3) {
            
             // Se encola el anuncio, pasándole el anuncio y su TIPO.
-            encolar(cola, cabezaAnuncio, TIPO_ANUNCIO);
+            encolar(colaReproduccion, cabezaAnuncio, TIPO_ANUNCIO);
+        
+            
+
             cabezaAnuncio=cabezaAnuncio->siguiente; // Avanzamos al siguiente anuncio.
-            cola->contadorCancionesFree = 0; // 5. Se reinicia el contador.
+            colaReproduccion->contadorCancionesFree = 0; // 5. Se reinicia el contador.
         }
     }
 }
 
-NodoCola* desencolar(NodoColadeReproduccion* cola) {
+NodoCola* desencolar(NodoColadeReproduccion *cola) {
     if (cola->frente == NULL) return NULL; // Cola vacía
 
     NodoCola* nodo = cola->frente;
