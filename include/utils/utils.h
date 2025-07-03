c8b6f0e3b33b5c275dd91ec34bfad3547aefcedb
andress-pabonm <andress.pabonm@ucuenca.edu.ec>
2025-07-02 23:45:59 -0500

    Incluido datatime.h en utils.h

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -12,5 +12,6 @@
 #include "async.h"         // Ejecución de procesos en paralelo
 #include "log.h"           // Escritura dinámica de strings
 #include "nodos/fntypes.h" // Para cmpfn_t y operfn_t
+#include "datetime.h"      // Para funciones de tiempo
 
 #endif // VIBECAST_UTILS_H
