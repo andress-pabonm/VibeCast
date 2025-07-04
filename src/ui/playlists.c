11ee07c372f5803c87df5b299bbf645d0da2b993
Pableis08 <040925pacg@gmail.com>
2025-07-03 22:35:11 -0500

    funcion de agregar cancion a playlist

--- src/ui/playlists.c
+++ src/ui/playlists.c
@@ -14,10 +14,43 @@ bool crearPlaylist(const char* nombre){
     return nueva;
 }
 
-bool agregarCancionPlaylist(){
-    //CREO YO QUE PARA NO CREAR TANTAS FUNCIONES HACER EN UNA SOLA, POR EJEMPLO
-    //EN ESTA FUNCION SE PODRIA AGREGAR CANCION EN UNA PLAYLIST PERO TAMBIEN PODRIA ELIMINARLA
-    //ASI NO CREAR DOS FUNCIONES
+bool agregarCancionPlaylist(Cancion* cancion, Playlist* playlist){
+    // Verifica que la canción y la playlist no sean nulas
+    // y que la lista de canciones de la playlist esté inicializada
+    if(!cancion || !playlist || !playlist->canciones) return false;
+
+    // Agrega la canción a la lista de canciones de la playlist
+    agregarALista(playlist->canciones, cancion);
+    return true;
+}
+
+void agregarALista(Lista lista, Cancion* cancion){
+    if (!lista || !cancion) return;  
+
+    Lista* nuevoNodo = newLista(cancion);
+
+    Lista* actual = lista->start;
+
+    if (!actual) {
+        lista->start = nuevoNodo; // Si la lista está vacía, el nuevo nodo es el inicio
+        return;
+
+    }else{
+        while (actual->next) {
+            actual = actual->next; // Recorre hasta el final de la lista
+        }
+        actual->next = nuevoNodo; // Agrega el nuevo nodo al final
+    }
+
+    cancion->popularidad++;
+
+    // Esta función debería agregar la canción a la lista
+    // Implementación de agregar a la lista
+}
+
+bool eliminarCancionPlaylist(Cancion* cancion, Playlist* playlist){
+    //SE PODRIA HACER CON UNA FUNCION QUE RECIBA EL ID DE LA CANCION A ELIMINAR
+    //Y LA PLAYLIST EN LA QUE SE QUIERE ELIMINAR
 }
 
 bool eliminarPlaylist(){
