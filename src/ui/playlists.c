1f7e94cc1168bfe94a19d2633a25aa26e87aa94e
JmateoAF <jostynmatius44@gmail.com>
2025-07-04 13:29:46 -0500

    Corregiendo playlist.c

--- src/ui/playlists.c
+++ src/ui/playlists.c
@@ -8,7 +8,12 @@ static new_cmpfn(cmpPlaylistConNombre)
 
     return strcmp(a->nombre, n);
 }
-
+new_operfn(restar_popularidad)
+{
+    Cancion *cancion = val;
+    cancion->popularidad--;
+    return FOREACH_CONTINUE; // constante que indica que se sigue iterando con el bucle de reccorer la lista 
+}
 bool crearPlaylist(const char *nombre)
 {
     Playlist *nuevaPlaylist = newPlaylist(); // Inicaliza como null
@@ -52,7 +57,7 @@ bool eliminarCancionPlaylist(int Idcancion, Playlist *playlist)
 {
 
     Cancion *cancionEliminada = deleteValueInLista(playlist->canciones, &Idcancion, cmpCancionConId);
-
+ 
     if (cancionEliminada == NULL)
     {
         printf("Error: La canción %s no se encuentra en la playlist\n", cancionEliminada->nombre);
@@ -61,6 +66,8 @@ bool eliminarCancionPlaylist(int Idcancion, Playlist *playlist)
     else
     {
         printf("Canción %s eliminada de la playlist '%s'\n", cancionEliminada->nombre, playlist->nombre);
+        // Restamos la popularidad de la canción eliminada
+        cancionEliminada->popularidad--;
         return true;
     }
 }
@@ -68,15 +75,6 @@ bool eliminarCancionPlaylist(int Idcancion, Playlist *playlist)
 bool eliminarPlaylist(const char *nombrePlaylist)
 {
     // Buscamos la playlist por nombre
-    Playlist *playlist = searchValueInLista(usuario->playlists, nombrePlaylist, cmpPlaylistConNombre);
-
-    if (!playlist)
-    {
-        printf("Error: La playlist '%s' no existe\n", nombrePlaylist);
-        return false;
-    }
-    else
-    {
         Playlist *playlisteliminada = deleteValueInLista(usuario->playlists, nombrePlaylist, cmpPlaylistConNombre); // Eliminamos la playlist de lalista de playlists
         if (playlisteliminada == NULL)
         {
@@ -85,30 +83,25 @@ bool eliminarPlaylist(const char *nombrePlaylist)
         }else{
             printf("Playlist '%s' eliminada correctamente\n", nombrePlaylist);
             // Liberamos la memoria asociada a la playlist
-            destroyLista(playlisteliminada->canciones, NULL, NULL); // Liberamos la lista de canciones
+            freem(playlisteliminada->nombre); // Liberamos el nombre de la playlist
+            destroyLista(playlisteliminada->canciones, restar_popularidad, NULL); // Liberamos la lista de canciones
             return true;
         }
-    }
-
-    // Eliminamos la playlist de la lista de playlists del usuario
-    // y liberamos la memoria asociada a la playlist
-    if (!usuario || !playlist || !usuario->playlists)
-        return false;
-
-    void *eliminado = deleteValueInLista(usuario->playlists, playlist, NULL);
-    return eliminado != NULL;
+    
 }
 
-void mostrarCancionesPlaylist()
-{
-    // SE MOTRARIA EN CONSOLA
-    int i = 1;
 
-    void mostrarCancion(void *_, int idx, void *val)
-    {
+
+void mostrarCancion(void *_, int idx, void *val) {
         Cancion *c = (Cancion *)val;
         printf("%d. %s - %s (%d segundos)\n", idx + 1, c->nombre, c->genero, c->duracion);
     }
+
+    
+void mostrarCancionesPlaylist(Playlist *playlist)
+{
+    // SE MOTRARIA EN CONSOLA
+    int i = 1; 
 
     forEachInLista(playlist->canciones, mostrarCancion, NULL);
 }
