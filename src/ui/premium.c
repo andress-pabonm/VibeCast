066bdd2a645ff7f70dec52487bf3246e717311e9
Pableis08 <040925pacg@gmail.com>
2025-07-05 15:17:11 -0500

    Actualizacion de Premium.c

--- src/ui/premium.c
+++ src/ui/premium.c
@@ -1,21 +1,61 @@
 #include <ui/interfaces.h>
 #include <utils/utils.h>
+#include <time.h>
 
-void activarPremium(){
-    usuario->plan = PLAN_PREMIUM; // Cambiamos el plan del usuario a premium
-    printf("¡Felicidades! Has activado el plan Premium.\n");
+typedef struct usuarioAUX{
+    void *Usuario; 
+    time_t caducidadPremium; // Fecha de caducidad del plan premium
+}usuarioAUX;
 
+void activarPremium(usuarioAUX *usuario) {
+
+    time_t base = time(NULL);
+
+    if (base == -1) {
+        printf("Error al obtener la fecha actual.\n");
+        return;
+    }
+    
+    Usuario *user = (Usuario *)usuario->Usuario;
+    if (user->plan != PLAN_PREMIUM) {
+        user->plan = PLAN_PREMIUM;
+        printf("¡Plan Premium activado! ");
+    } else {
+        base = usuario->caducidadPremium;
+        printf("¡Plan Premium renovado! ");
+    }
+
+    usuario->caducidadPremium = base + 30 * 24 * 60 * 60; //Tiempo en segundos
+    //La fecha es imprime como dia de la semana, mes, dia del mes, hora, minuto, segundo y año
+    printf("Caduca el: %s", ctime(&usuario->caducidadPremium));
 }
-void desactivarPremium(){
 
-    usuario->plan = PLAN_FREEMIUM; // Cambiamos el plan del usuario a freemium
+void desactivarPremium(usuarioAUX *usuario) {
+
+    Usuario *user = (Usuario *)usuario->Usuario;
+    user->plan = PLAN_FREEMIUM; // Cambiamos el plan del usuario a freemium
+    usuario->caducidadPremium = 0; // Reseteamos la fecha de caducidad del plan premium
     printf("Has desactivado el plan Premium. Ahora estás en el plan Freemium.\n");
     
 }
-bool isPremium(){
-    return usuario->plan == PLAN_PREMIUM; // Retorna true si el usuario es premium
+bool isPremium(usuarioAUX *usuario) {
+
+    time_t ahora = time(NULL);
+    if (ahora == -1) {
+        printf("Error al obtener la fecha actual.\n");
+        return false;
+    }
+
+    if(usuario->caducidadPremium <= ahora){
+        printf("Tu plan Premium ha caducado. Por favor, renueva tu suscripción.\n");
+        desactivarPremium(usuario); // Desactiva el plan si ha caducado
+        return false; // Retorna false si el usuario no es premium
+    }
+
+    Usuario *user = (Usuario *)usuario->Usuario;
+    return user->plan == PLAN_PREMIUM; // Retorna true si el usuario es premium
 }
-void renovarpremium(){
-    activarPremium(); // Llama a la función para activar el plan premium
+void renovarpremium(usuarioAUX *usuario) {
+    activarPremium(usuario); // Llama a la función para activar el plan premium
     printf("Tu plan Premium ha sido renovado exitosamente.\n");
 }
