c2cdbb2008c3ac229cbfd60df3de056cfe0b0aaa
JmateoAF <jostynmatius44@gmail.com>
2025-07-05 14:50:46 -0500

    La paula es lesbiana

--- src/ui/estadisticasReportes.c
+++ src/ui/estadisticasReportes.c
@@ -2,8 +2,8 @@
 
 typedef struct PopularidadArtista
 {
-    const char *artista;           // Artista
-    int numCancionesGuardadas = 0; // Cantidad de reproducciones
+    const char *artista;       // Artista
+    int numCancionesGuardadas; // Cantidad de reproducciones
 } PopularidadArtista;
 
 new_cmpfn(cmpIntDesc)
@@ -51,7 +51,7 @@ new_operfn(ObtenerHistorial)
         artistaTemp->artista = reproducciontemp->cancion->album->artista;
 
         PopularidadArtista *artistas = searchValueInLista(artistas, artistaTemp->artista, cmpArtistaConNombre); // Buscamos el artista en la lista de artistas
-                                                                                                                // comentario random del segundo dia comiendo chaulafan//
+                                                                                                                // comentario random del segundo dia comiendo chaulafan
         if (artistas)                                                                                           // Si el artista no esta en la lista de artistas
         {
             artistas->numCancionesGuardadas++;
@@ -81,9 +81,21 @@ new_operfn(ObtenerHistorial)
     return FOREACH_CONTINUE; // Continuar recorriendo la lista de amigos
 }
 
+new_operfn(obtener_artistas)
+{
+    PopularidadArtista *artistas = val;
+    int *guardados = arg;
+
+    if (artistas)
+        guardados[idx] = artistas->numCancionesGuardadas; // Guardamos la popularidad en el vector
+
+    return FOREACH_CONTINUE; // Continuar iterando
+}
+
 void generarTop5Canciones()
 {
     int longitud = getListaLength(canciones);
+
     if (longitud == 0)
     {
         printf("No hay canciones disponibles para generar el top 5\n");
@@ -99,7 +111,7 @@ void generarTop5Canciones()
 
     FILE *archivo = newFile("top5_canciones.txt", NULL);
 
-    fprintf(archivo, "Top 5 Canciones más reproducidas:\n");
+    fprintf(archivo, "Top 5 Canciones más reproducidas:\n\n");
 
     for (int i = 0; i < longitud && i < 5; i++)
     {
@@ -111,37 +123,34 @@ void generarTop5Canciones()
 
 void generarTop3Artistas()
 {
-    int longitud = getListaLength(canciones);
+    PopularidadArtista *listaArtistas; // Crear una nueva lista para los artistas
+    int longitud = getListaLength(listaArtistas);
 
     if (longitud == 0)
     {
-        printf("No hay canciones disponibles para generar el top 5\n");
+        printf("No hay astistas disponibles\n");
 
         return;
     }
 
-    int *reproducciones = malloc_cpy(longitud * sizeof(int), 0); // vector en el heap
+    int *artistas = malloc_cpy(longitud * sizeof(int), 0); // vector en el heap
 
-    forEachInLista(canciones, obtener_reproducciones, reproducciones);
+    forEachInLista(listaArtistas, obtener_artistas, artistas);
 
-    qsort(reproducciones, longitud, sizeof(int), cmpIntDesc); // Buffer, tamaño del buffer, tamaño de cada elemento, funcion de comparacion
+    qsort(artistas, longitud, sizeof(int), cmpIntDesc); // Buffer, tamaño del buffer, tamaño de cada elemento, funcion de comparacion
 
-    FILE *archivo = newFile("top5_canciones.txt", NULL);
+    FILE *archivo = newFile("top3_artitas.txt", NULL);
 
-    fprintf(archivo, "Top 5 Canciones más reproducidas:\n");
+    fprintf(archivo, "Top 3 artistas mas preferidos:\n");
 
-    for (int i = 0; i < longitud && i < 5; i++)
+    for (int i = 0; i < longitud && i < 3; i++)
     {
-        fprintf(archivo, "%d) %d reproducciones\n", i + 1, reproducciones[i]);
+        fprintf(archivo, "%d) %s - %d canciones guardadas\n", i + 1, listaArtistas[i].artista, artistas[i]);
+
+        // Top 3 artistas mas preferidos:
+
+        // 1) Juanes - 10 canciones guardadas
     }
 
     fclose(archivo);
-}
-
-Lista recomendarCanciones()
-{
-    Lista ListaCancionesRecomendadas = newLista(NULL); // Crear una nueva lista para las canciones recomendadas
-    forEachInLista(usuario->amigos, ObtenerHistorial, ListaCancionesRecomendadas);
-    // recorrer la lista de amigos y llamar a la funcion recomendar_canciones
-    return ListaCancionesRecomendadas; // Retornar la lista de canciones recomendadas
 }
