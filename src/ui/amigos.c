cef1aca7aec827305cc2658c020658aba91650f4
JmateoAF <jostynmatius44@gmail.com>
2025-07-06 11:30:14 -0500

    Diseño 'final' de amigos.c

--- src/ui/amigos.c
+++ src/ui/amigos.c
@@ -1,5 +1,40 @@
 #include <ui/interfaces.h>
 
+new_operfn(ObtenerHistorial)
+{
+    Usuario *u = val;                       // Es un puntero a usuario que esta en la lista de amigos
+    Lista ListaCancionesRecomendadas = arg; // Argumento para la funcion de reccorrer la lista de amigos
+
+    Pila historial = u->historial.reproducciones;
+    Pila TempHistorial = newPila();
+
+    // Obtenemos la pila de reproducciones del historial del usuario
+    Cancion *canciontemp;
+    Reproduccion *reproducciontemp = deleteValueInPila(historial);
+
+    while (reproducciontemp)
+    {
+        // Recorremos la pila de reproducciones del usuario
+        canciontemp = searchValueInLista(canciones, &reproducciontemp->cancion->id, cmpCancionConId);
+        insertValueInLista(ListaCancionesRecomendadas, canciontemp);
+
+        insertValueInPila(TempHistorial, reproducciontemp); // Insertamos la reproduccion en la pila temporal
+        reproducciontemp = deleteValueInPila(historial);    // Obtenemos la siguiente reproduccion
+    }
+
+    reproducciontemp = deleteValueInPila(TempHistorial); // Obtenemos la ultima reproduccion
+
+    while (reproducciontemp)
+    {
+        insertValueInPila(historial, reproducciontemp);
+        reproducciontemp = deleteValueInPila(TempHistorial); // Insertamos las reproducciones de la pila temporal en la pila de reproducciones del historial
+    }
+
+    destroyPila(TempHistorial, NULL, NULL); // Destruimos la pila temporal
+
+    return FOREACH_CONTINUE; // Continuar recorriendo la lista de amigos
+}
+
 void agregarAmigo(const char *username)
 {
     // Obtener lista de amigos del usuario actual
@@ -41,7 +76,7 @@ void eliminarAmigo(const char *username)
     {
         // Eliminar de la lista de amigos
         deleteValueInLista(amigos, amigo->username, cmpUsuarioConUsername);
-        printf("El usuario %s ha sido eliminado de tu lista de amigos.\n", amigo->username);
+        printf("El usuario %s ha sido eliminado de tu lista de amigos\n", amigo->username);
     }
     else
     {
@@ -49,47 +84,10 @@ void eliminarAmigo(const char *username)
     }
 }
 
-new_operfn(ObtenerHistorial)
-{
-    Usuario *u = val;                       // val=void *val, que es un puntero a Usuario que estan en la lista de amigos
-    Lista ListaCancionesRecomendadas = arg; // argumento para la funcion de reccorrer la lista de amigos
-
-    Pila historial = u->historial.reproducciones;
-    Pila TempHistorial = newPila();
-
-    // Obtenemos la pila de reproducciones del historial del usuario
-    Cancion *canciontemp;
-    Reproduccion *reproducciontemp = deleteValueInPila(historial);
-
-    while (reproducciontemp)
-    {
-        // Recorremos la pila de reproducciones del usuario
-        canciontemp = searchValueInLista(canciones, &reproducciontemp->cancion->id, cmpCancionConId);
-        insertValueInLista(ListaCancionesRecomendadas, canciontemp);
-
-        insertValueInPila(TempHistorial, reproducciontemp); // Insertamos la reproduccion en la pila temporal
-        reproducciontemp = deleteValueInPila(historial);    // Obtenemos la siguiente reproduccion
-    }
-
-    reproducciontemp = deleteValueInPila(TempHistorial); // Obtenemos la ultima reproduccion
-
-    while (reproducciontemp)
-    {
-        insertValueInPila(historial, reproducciontemp);
-        reproducciontemp = deleteValueInPila(TempHistorial); // Insertamos las reproducciones de la pila temporal en la pila de reproducciones del historial
-    }
-
-    destroyPila(TempHistorial, NULL, NULL); // Destruimos la pila temporal
-
-    return FOREACH_CONTINUE; // Continuar recorriendo la lista de amigos
-}
-
-Lista recomendarCanciones()
+Lista *recomendarCanciones()
 {
     Lista ListaCancionesRecomendadas = newLista(NULL); // Crear una nueva lista para las canciones recomendadas
     forEachInLista(usuario->amigos, ObtenerHistorial, ListaCancionesRecomendadas);
-    // recorrer la lista de amigos y llamar a la funcion recomendar_canciones
+
     return ListaCancionesRecomendadas; // Retornar la lista de canciones recomendadas
 }
-
-/* ================================================================ */
